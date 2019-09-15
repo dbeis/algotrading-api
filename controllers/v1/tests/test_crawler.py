81e98f19285cf16acbc3aae6c9432a73f954a709
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-09-15 12:32:44 +0300

    noresources please type  and fix module errors idk wtf is going on with test imports

---
+++ controllers/v1/tests/test_crawler.py
@@ -0,0 +1,34 @@
+import flask
+import controllers.v1.crawler as controller
+from contracts.v1 import requests
+from controllers.common import ok, error, not_found
+from entities import *
+import api
+
+def test_insert_data_correct(mocker):   
+    # arrange
+    data_to_insert = requests.CrawledSocialDataRequest([
+        requests.CrawledSocialDataRecord('cid0', 'content0', 123.4, ['tag0', 'tag1']),
+        requests.CrawledSocialDataRecord('cid1', 'content1', 123.4, ['tag1', 'tag2'])
+    ])
+
+    expected_entities_inserted = [models.CrawledSocialDataEntity(cid = x.cid, content = x.content, timestamp = x.timestamp)
+        for x in data_to_insert]
+
+    expected_tags_inserted = [models.CrawledSocialDataEntityTags(tag = t, cid = x.cid)
+        for x in data_to_insert
+        for t in x.tags]
+
+    request_mock = mocker.patch.object(flask, 'request')
+    request_mock.data = data_to_insert.serialize()
+    db_mock = request_mock = mocker.patch.object(api, 'db')
+
+    # act
+    result = controller.insert_data()
+
+    # assert
+
+    db_mock.session.add_all.assert_called_with(*expected_entities_inserted, *expected_tags_inserted)
+    db_mock.session.commit.assert_any_call()
+
+    assert result == ok()
